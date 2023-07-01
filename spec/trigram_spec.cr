ccdaa4a172517ddc3056b4aca8a5a5a29c8d5c4c
devnote-dev <devnote.dev75@gmail.com>
2023-07-01 16:48:18 +0100

    feat(spec): implement proper specs

--- spec/trigram_spec.cr
+++ spec/trigram_spec.cr
@@ -4,5 +4,45 @@ require "../src/trigram"
 describe Trigram do
   it "parses simple strings" do
     Trigram.parse("Cod").should eq [4419428]
+    Trigram.parse("Code").should eq [4419428, 7300197]
+  end
+
+  it "parses basic queries" do
+    trigram = Trigram.new do |t|
+      t.add "Code is my life"
+      t.add "Search"
+      t.add "I write a lot of Codes"
+    end
+
+    res = trigram.query "Code"
+    res.size.should eq 2
+    res.should eq [1, 3]
+  end
+
+  it "parses empty or small strings" do
+    trigram = Trigram.new do |t|
+      t.add "Code is my life"
+      t.add "Search"
+      t.add "I write a lot of Codes"
+    end
+
+    res = trigram.query "te"
+    res.size.should eq 3
+    res.should eq [1, 2, 3]
+
+    res = trigram.query ""
+    res.size.should eq 3
+    res.should eq [1, 2, 3]
+  end
+
+  it "deletes an index" do
+    trigram = Trigram.new &.add "Code is my life"
+
+    trigram.delete "Code", 1
+    trigram.query("Code").should be_empty
+
+    res = trigram.query "life"
+    res.size.should eq 1
+    res.should eq [1]
   end
 end
